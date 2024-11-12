d5412f994e5e5fe1c253c96a638576cdb050d5e0
eileen <yw.eileen@gmail.com>
2024-11-13 00:04:03 +0800

    update-method

--- src/test/java/com/demo/DemoTest.java
+++ src/test/java/com/demo/DemoTest.java
@@ -1,6 +1,10 @@
 package com.demo;
 
 import com.demo.pages.Homepage;
+import com.demo.pages.SearchResultPage;
+import com.demo.utils.CSVReaderUtils;
+import com.fasterxml.jackson.annotation.JacksonInject;
+import com.opencsv.exceptions.CsvValidationException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -11,42 +15,64 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 @Listeners(TestListener.class)
 
 public class DemoTest {
 
     private WebDriver driver;
 
+    private CSVReaderUtils csvReaderUtils;
+    private SearchResultPage searchPage;
+    private Homepage homepage;
+
     @BeforeClass
     public void setup() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
 
     @Test
-    public void testOpenHomePage() {
-        WebDriverWait wait = new WebDriverWait(driver, 5);
-
+    public void testOpenHomePage() throws InterruptedException {
         driver.get("https://www.zurich.com.my/");
-        Homepage homepage = new Homepage(driver);
-
+        homepage = new Homepage(driver);
+        SearchResultPage searchPage = new SearchResultPage(driver);
         Assert.assertEquals(homepage.getTitle(), "Home | Zurich Malaysia");
 //        wait.until(ExpectedConditions.visibilityOf(homepage.));
         homepage.clickCloseBtn();
         homepage.clickSearchButton();
-        homepage.
-//        homepage.clickMenuList();
-//        homepage.clickBuyOnlineLink();
+        homepage.searchKeyword("contacts");
+        homepage.clickSearchEnter();
+        Thread.sleep(20000);
+        Assert.assertEquals(searchPage.getSearchPageURL(), "https://www.zurich.com.my/search?keyphrase=contacts&page=1");
+
     }
-//    @Test
-//     public void testOpenBuyOnlinePage(){
-//        BuyOnlinePage buyOnlinePage = new BuyOnlinePage(driver);
-//        Assert.assertEquals(buyOnlinePage.getTitle(),"Careers | Zurich Malaysia");
-//        Assert.assertEquals(buyOnlinePage.getPageURL(),"https://www.zurich.com.my/online-products");
-//   }
+
+    @Test
+    public void testSearchPage() throws InterruptedException{
+        driver.get("https://www.zurich.com.my/search");
+        csvReaderUtils = new CSVReaderUtils();
+        List<String[]> searchKeywordsList = csvReaderUtils.getTestData("src/resources/data/data.csv");
+        for (String[] searchKeywords : searchKeywordsList) {
+            for (String keyword : searchKeywords) {
+                searchPage = new SearchResultPage(driver);
+                String[] splitKeywords = keyword.split(",");
+                Thread.sleep(5000);
+                searchPage.inputSearchTextbox(splitKeywords[0].toString());
+//                searchPage.clickOnSearchBtn();
+                Thread.sleep(5000);
+                Assert.assertEquals(searchPage.getSearchPageURL(), splitKeywords[1].toString());
+            }
+
+        }
+    }
 
     @AfterClass
     public void tearDown() {
